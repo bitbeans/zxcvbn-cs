af7b4410d641e0dcc4b5f619701d107a7a384d4d
Michael Ford <michael@ford.id.au>
2013-03-05 22:25:57 +1100

    Initial commit of project files.

---
+++ Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// TODO: These should probably be immutable
+namespace Zxcvbn
+{
+    public class Result
+    {
+        public double Entropy { get; set; }
+        public int CrackTime { get; set; }
+        public string CrackTimeDisplay { get; set; }
+        public int Score { get; set; }
+        public IList<Match> MatchSequence { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Match
+    {
+        public string Pattern { get; set; }
+        public string Token { get; set; }
+        public double Entropy { get; set; }
+        public int Cardinality { get; set; }
+
+        // TODO: Rename, or make internal?
+        public int i { get; set; }
+        public int j { get; set; }
+    }
+
+}
---
+++ Zxcvbn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zxcvbn
+{
+    public class Zxcvbn
+    {
+        public static Result MatchPassword(string password)
+        {
+            return MatchPassword(password, new string[0]);
+        }
+
+        public static Result MatchPassword(string password, IEnumerable<string> userInputs)
+        {
+            return new Result();
+        }
+
+    }
+}
